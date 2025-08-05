04626118690c9c7a85a0577a0d8c73615d5f89e0
razon30 <hossainmdrazon@gmail.com>
2025-08-05 15:44:25 +1000

    changed in testing and workflows

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://54.206.217.151', // live
+  baseURL: 'http://54.206.217.151:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
